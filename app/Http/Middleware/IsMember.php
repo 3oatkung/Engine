f25d1d5a73facfe154b939ccaab76c5b01ef1f88
3oatkung <masterphaisit@hotmail.com>
2016-01-29 12:13:07 +0700

    Update Payment

--- app/Http/Middleware/IsMember.php
+++ app/Http/Middleware/IsMember.php
@@ -25,8 +25,7 @@ class IsMember
         }
         elseif(count(Payment::where('email',Auth::user()->email)->get())>0)
         {
-            $users = User::where('id',Auth::user()->email)->get();
-            dd($users);
+            $users = User::where('email',Auth::user()->email)->get();
             $user = $users[0];
             $user->memberStatus == 'Member';
             $user->save();
